463a266986f333e3c439ac739b4e383d547e0020
Sarfraz <sarfraznawaz2005@gmail.com>
2025-07-24 18:41:28 +0500

    updated

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,23 +3,15 @@
 @section('content')
     <h3>Dashboard</h3>
     <div class="row">
-        <div class="col-md-3">
+        <div class="col-md">
             <div class="card text-white bg-primary mb-3">
-                <div class="card-header">Total Tenants</div>
-                <div class="card-body">
-                    <h5 class="card-title">{{ $totalTenants }}</h5>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card text-white bg-info mb-3">
                 <div class="card-header">Total Rents</div>
                 <div class="card-body">
                     <h5 class="card-title">{{ $totalRents }}</h5>
                 </div>
             </div>
         </div>
-        <div class="col-md-3">
+        <div class="col-md">
             <div class="card text-white bg-success mb-3">
                 <div class="card-header">Total Amount Received</div>
                 <div class="card-body">
@@ -27,7 +19,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-3">
+        <div class="col-md">
             <div class="card text-white bg-danger mb-3">
                 <div class="card-header">Total Amount Remaining</div>
                 <div class="card-body">
